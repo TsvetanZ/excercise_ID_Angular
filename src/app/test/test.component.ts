3952f7ac14b0454bebef17653e09280bce21fcf8
Tsvetan Zahariev <102287382+TsvetanZ@users.noreply.github.com>
2022-11-16 20:00:28 +0200

    add change

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,28 @@
+
+import { ChangeDetectionStrategy,ChangeDetectorRef,Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TestComponent implements OnInit, OnChanges {
+
+  @Input() users!: {name: string }[] 
+
+  constructor(private cdRef: ChangeDetectorRef) { 
+    this.cdRef.detach();
+  }
+
+  ngOnChanges(): void {
+    if( this.users.length > 4) {
+      this.cdRef.detectChanges();
+    }
+  }
+
+  ngOnInit(): void {
+    this.cdRef.detectChanges();
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,10 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// monkey patching това се заменя от zone.js
+ // const _setInterval = window.setInterval;
+ // (window as any). setInterval = function (...args: any[]): number {
+ //   console.log('setInterval was called', args);
+ //   return _setInterval.apply(this, args as any) as unknown as number;
+ // }
